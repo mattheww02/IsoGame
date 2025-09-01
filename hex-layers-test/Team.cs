4a164e25d7fac63d72a37eb93710f0e748049a1a
mattheww02 <78615724+mattheww02@users.noreply.github.com>
2025-09-01 22:01:53 +0100

    Add enemy movement

--- hex-layers-test/Team.cs
+++ hex-layers-test/Team.cs
@@ -10,6 +10,36 @@ namespace HexLayersTest;
 public class Team
 {
     public Color GuiColour { get; set; }
+    public bool IsPlayerControlled { get; set; }
 
-    private HashSet<Unit> _units;
+    private readonly HashSet<Unit> _units;
+
+    public IEnumerable<Unit> Units => _units;
+
+    public Team()
+    {
+        _units = [];
+    }
+    
+    public bool AddUnit(Unit unit)
+    {
+        if (_units.Add(unit))
+        {
+            unit.Team = this;
+            unit.Modulate = GuiColour;
+            return true;
+        }
+        return false;
+    }
+
+    public bool RemoveUnit(Unit unit)
+    {
+        if (_units.Remove(unit))
+        {
+            unit.Team = null;
+            unit.Modulate = Colors.White;
+            return true;
+        }
+        return false;
+    }
 }
